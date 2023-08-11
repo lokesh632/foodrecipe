4bb0246b5a2241fe422c67d784c0578fc2397c37
Lokesh kumar <66361415+lokesh632@users.noreply.github.com>
2023-08-11 13:16:44 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Common/Navbar';
+import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
